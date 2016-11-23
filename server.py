320c0a681a52950d7351086379ec7dabfa385ae6
Miguel A. Angulo <maangulo12@gmail.com>
2016-11-23 00:36:41 -0600

    Update template folder

--- server.py
+++ server.py
@@ -10,7 +10,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder='frontend', static_url_path='', static_folder='')
+app = Flask(__name__, template_folder='frontend/www', static_url_path='', static_folder='')
 
 
 @app.route('/')
