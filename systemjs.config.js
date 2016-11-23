7a7bdc67d6d1242dae6e4a9627daddbe0837fde7
Miguel A. Angulo <maangulo12@gmail.com>
2016-11-23 00:35:51 -0600

    Update systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'frontend/app',
+      app: 'frontend/www/build/app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
