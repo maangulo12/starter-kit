087d51653ba63b9013ab030abe8974b732a20058
Miguel A. Angulo <maangulo12@gmail.com>
2016-11-23 14:16:39 -0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
+'use strict';
+
 var gulp        = require('gulp');
 var execute     = require('child_process').exec;
-var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var concat      = require('gulp-concat');
 var tsc         = require('gulp-typescript');
 var tscConfig   = require('./tsconfig.json');
+var browserSync = require('browser-sync').create();
 
 // Run the server
 gulp.task('runserver', function() {
-    var proc = execute('python3 server.py');
-    // For non-linux, use this
-    // var proc = execute('python server.py');
+  var proc = execute('python3 server.py');
+  // For non-linux, use this
+  // var proc = execute('python server.py');
 });
 
-// Compile the typescript files
+// Copy the HTML templates
+gulp.task('html-copy', function() {
+  return gulp
+    .src('frontend/src/**/*.html')
+    .pipe(gulp.dest('frontend/www/build'));
+});
+
+// Compile the SASS files 
+gulp.task('sass-compile', function() {
+  return gulp
+    .src('frontend/src/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(concat('all.min.css'))
+    .pipe(gulp.dest('frontend/www/build'));
+});
+
+// Compile the TypeScript files
 gulp.task('ts-compile', function() {
   return gulp
     .src('frontend/src/**/*.ts')
@@ -19,19 +39,13 @@ gulp.task('ts-compile', function() {
     .pipe(gulp.dest('frontend/www/build'));
 });
 
-// Copy the HTML templates
-gulp.task('cp-templates', function () {
-    return gulp
-      .src('frontend/src/**/*.html')
-      .pipe(gulp.dest('frontend/www/build'));
-});
-
 // Default run command
-gulp.task('default', ['runserver', 'cp-templates', 'ts-compile'], function() {
+gulp.task('default', ['runserver', 'html-copy', 'sass-compile', 'ts-compile'], function() {
   browserSync.init({ proxy: 'localhost:5000' });
 
   // Watchers
-  gulp.watch(['frontend/src/**/*.html'], ['cp-templates']);
-  gulp.watch(['frontend/src/**/*.ts'], ['ts-compile']);
-  gulp.watch(['frontend/**/*.*'], browserSync.reload);
+  gulp.watch('frontend/src/**/*.html', ['html-copy']);
+  gulp.watch('frontend/src/**/*.scss', ['sass-compile']);
+  gulp.watch('frontend/src/**/*.ts',   ['ts-compile']);
+  gulp.watch('frontend/**/*.*', browserSync.reload);
 });
