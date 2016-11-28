4e924831cbfef3eafeac9cc0c4c0ea22c81ee810
Miguel A. Angulo <maangulo12@gmail.com>
2016-11-28 14:52:34 -0600

    Update server module

--- server.py
+++ server.py
@@ -4,44 +4,12 @@
     server.py
     ~~~~~~~~~
 
-    This is the main module of this application.
-
-    Flask extensions included in this application:
-    - Flask-SQLAlchemy : Used for creating database models (using SQLAlchemy).
-    - Flask-Bcrypt     : Used for hashing the passwords in the database.
-    - Flask-Script     : Used for adding support for command-line tasks.
+    This module is used to run the application.
 """
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_script import Manager
-
-
-# Creating the Flask app
-app = Flask(__name__,
-            template_folder='frontend/www',
-            static_url_path='',
-            static_folder='')
-
-# Configuring the app from config module
-app.config.from_pyfile('backend/config.py')
-
-# Initializing the Flask extensions
-db      = SQLAlchemy(app)
-bcrypt  = Bcrypt(app)
-manager = Manager(app)
-
-# Importing the database models
-from backend import models
-
-# Importing the app views
-from backend import views
-
-# Importing the app manager commands
-from backend import commands
+from backend import app
 
 
 if __name__ == "__main__":
-    # Running the app
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Run the application
+    app.run(host='0.0.0.0', port=5000)
