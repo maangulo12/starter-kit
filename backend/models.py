6e9f0d844d804b4c98b81de3a87504e5ed4e7f37
Miguel A. Angulo <maangulo12@gmail.com>
2016-11-28 13:09:52 -0600

    Update models module

---
+++ backend/models.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    backend.models
+    ~~~~~~~~~~~~~~
+
+    This module implements the database models of this application.
+
+    For more information on how to create models:
+        - Flask-SQLAlchemy : http://flask-sqlalchemy.pocoo.org/2.1/
+        - SQLAlchemy       : http://www.sqlalchemy.org/
+
+    For more information on how to hash passwords:
+        - Flask-Bcrypt : https://flask-bcrypt.readthedocs.io/en/latest/
+"""
+
+from server import db, bcrypt
+
+
+class User(db.Model):
+    """This is the first model in our database."""
+    __tablename__ = 'users'
+
+    # Fields
+    id       = db.Column(db.Integer, primary_key=True)
+    email    = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def __init__(self, email, username, password):
+        """
+        This function initializes this model. This function is necessary
+        since we are hashing the user's password before storing it into 
+        the database.
+        """
+        self.email    = email
+        self.username = username
+        # Protecting the user's password using a hash function
+        self.password = bcrypt.generate_password_hash(password)
+    
+    def check_password(self, password):
+        """This is a helper function for checking the user's password."""
+        return bcrypt.check_password_hash(self.password, password)
