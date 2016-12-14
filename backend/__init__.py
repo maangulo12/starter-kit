f6e3b8239294eeaeadf05f56f076869b88624c4a
Miguel Angulo <maangulo12@gmail.com>
2016-12-13 22:57:52 -0600

    Update init module

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,14 +7,18 @@
     This is the main module of this application.
 
     Flask extensions included in this application:
-    - Flask-SQLAlchemy : Used for creating database models (using SQLAlchemy).
     - Flask-Bcrypt     : Used for hashing passwords.
+    - Flask-Migrate    : Used for performing SQLAlchemy database migrations.
+    - Flask-Restless   : Used for easy RESTful API generation.
     - Flask-Script     : Used for adding support for command-line tasks.
+    - Flask-SQLAlchemy : Used for creating database models (using SQLAlchemy).
 """
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+from flask_migrate import Migrate
 from flask_script import Manager
 
 
@@ -28,15 +32,14 @@ app = Flask(__name__,
 app.config.from_pyfile('config.py')
 
 # Initializing the Flask extensions
-db      = SQLAlchemy(app)
-bcrypt  = Bcrypt(app)
-manager = Manager(app)
+bcrypt   = Bcrypt(app)
+db       = SQLAlchemy(app)
+restless = APIManager(app, flask_sqlalchemy_db=db)
+migrate  = Migrate(app, db)
+manager  = Manager(app)
 
 # Importing the database models
 from backend import models
 
 # Importing the app views
 from backend import views
-
-# Importing the app manager commands
-from backend import commands
