f2b43ea666b506ec8062fe4aca62811848d7190e
Miguel Angulo <maangulo12@gmail.com>
2016-12-22 20:43:27 -0600

    Update login module

---
+++ frontend/src/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'login-cmp',
+  templateUrl: 'login.html'
+})
+export class LoginComponent { 
+  name = 'LoginComponent'; 
+}
