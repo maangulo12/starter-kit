f8a2b04b5dad5d137f2e440d8af8fcfe588e1a15
Miguel Angulo <maangulo12@gmail.com>
2016-12-22 20:43:07 -0600

    Update app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,26 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }   from './app.component';
+import { HomeComponent } from '../pages/home/home';
+import { LoginComponent } from '../pages/login/login';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [ 
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
