15dcf2d870538429cac78ed4d9fa48f92332a963
Miguel Angulo <maangulo12@gmail.com>
2016-12-22 20:43:47 -0600

    Update systemjs

--- frontend/www/systemjs.config.js
+++ frontend/www/systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'frontend/www/build/app',
+      app: 'frontend/www/build',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -33,7 +33,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
+        main: './app/main.js',
         defaultExtension: 'js'
       },
       rxjs: {
