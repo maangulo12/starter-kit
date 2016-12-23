0a384b8e5054658ec5feef53c529d6a591be4ef0
Miguel Angulo <maangulo12@gmail.com>
2016-12-22 20:43:19 -0600

    Add home module

---
+++ frontend/src/pages/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'home-cmp',
+  templateUrl: 'home.html'
+})
+export class HomeComponent { 
+  name = 'HomeComponent'; 
+}
