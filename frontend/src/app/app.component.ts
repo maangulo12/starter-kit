efe00964bbfef2c5aa6c0302ecfd2f3a10931f6b
Miguel Angulo <maangulo12@gmail.com>
2016-12-22 20:42:59 -0600

    Update app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent {}
