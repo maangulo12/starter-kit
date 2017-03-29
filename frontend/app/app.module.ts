b9f1c5a99eddc252dc46865e811de88d9dba55e3
Miguel A. Angulo <maangulo12@gmail.com>
2017-03-28 21:18:15 -0500

    Added FormsModule

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }          from '@angular/forms';
 
 import { AppComponent }   from './app.component';
 import { HomeComponent }  from './home/home';
@@ -14,7 +15,8 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [ 
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   declarations: [ 
     AppComponent,
