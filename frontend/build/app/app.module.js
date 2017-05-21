07fc4b1ff4ccd2c321bcc4671d93e3c6cf8ec609
Miguel A. Angulo <maangulo12@gmail.com>
2017-05-21 14:10:09 -0500

    Update app

--- frontend/build/app/app.module.js
+++ frontend/build/app/app.module.js
@@ -5,15 +5,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var router_1 = require('@angular/router');
-var app_component_1 = require('./app.component');
-var home_1 = require('./home/home');
-var login_1 = require('./login/login');
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var router_1 = require("@angular/router");
+var forms_1 = require("@angular/forms");
+var app_component_1 = require("./app.component");
+var home_1 = require("./home/home");
+var login_1 = require("./login/login");
 var appRoutes = [
     { path: '', component: home_1.HomeComponent },
     { path: 'login', component: login_1.LoginComponent }
@@ -21,21 +20,21 @@ var appRoutes = [
 var AppModule = (function () {
     function AppModule() {
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                router_1.RouterModule.forRoot(appRoutes)
-            ],
-            declarations: [
-                app_component_1.AppComponent,
-                home_1.HomeComponent,
-                login_1.LoginComponent
-            ],
-            bootstrap: [app_component_1.AppComponent]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppModule);
     return AppModule;
 }());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [
+            platform_browser_1.BrowserModule,
+            router_1.RouterModule.forRoot(appRoutes),
+            forms_1.FormsModule
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            home_1.HomeComponent,
+            login_1.LoginComponent
+        ],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
 exports.AppModule = AppModule;
