627a3f404c4b5db66131cf2b48334915c17c1950
Miguel A. Angulo <maangulo12@gmail.com>
2017-06-28 13:10:28 -0500

    Add users service

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,12 +1,19 @@
+// Angular Modules
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
 
+// App Components
 import { AppComponent }   from './app.component';
 import { HomeComponent }  from './home/home';
 import { LoginComponent } from './login/login';
 
+// App Services
+import { UsersService }   from './services/users.service';
+
+// App Routes
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent }
@@ -16,12 +23,16 @@ const appRoutes: Routes = [
   imports: [ 
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   declarations: [ 
     AppComponent,
     HomeComponent,
     LoginComponent
+  ],
+  providers: [
+    UsersService
   ],
   bootstrap: [ AppComponent ]
 })
